aba5abe2b311ec0313a2a4037238daf11f49d043
husobj <husobj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-14 09:15:08 +0000

    Added GPL License info.
    git-svn-id: https://plugins.svn.wordpress.org/google-404/trunk@384727 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- google-404.php
+++ google-404.php
@@ -2,11 +2,28 @@
 
 /*
 Plugin Name: Google 404
-Plugin URI: http://wordpress.org/extend/plugins/google-404/
+Plugin URI: http://www.benhuson.co.uk/wordpress-plugins/google-404/
 Description: Adds a Google 404 widget to your missing 'page not found' pages.
-Author: Ben Huson
 Version: 1.0.2
+Author: Ben Huson
 Author URI: http://www.benhuson.co.uk/
+License: GPLv2 or later
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 class Google404 {
